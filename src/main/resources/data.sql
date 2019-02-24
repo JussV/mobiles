db91500a65b82b714f8326b7113c486683b60471
JussV <joana.veljanoska@gmail.com>
2019-02-24 15:11:23 +0100

    Extend seed file to insert data in mobile_subscription

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
-INSERT INTO customer (id, first_name, last_name) VALUES (1000, 'Joana', 'Goshevska');
-INSERT INTO customer (id, first_name, last_name) VALUES (1001, 'John', 'Doe');
-INSERT INTO customer (id, first_name, last_name) VALUES (1002, 'Axl', 'Rose');
+INSERT INTO CUSTOMER (id, first_name, last_name) VALUES (1000, 'Joana', 'Goshevska');
+INSERT INTO CUSTOMER (id, first_name, last_name) VALUES (1001, 'John', 'Doe');
+INSERT INTO CUSTOMER (id, first_name, last_name) VALUES (1002, 'Axl', 'Rose');
+
+INSERT INTO MOBILE_SUBSCRIPTION (msisdn, customer_id_owner, customer_id_user, service_type, service_start_date)
+  VALUES ('+35677141310', 1000, 1000, 'MOBILE_POSTPAID', DATEDIFF('SECOND', DATE '1970-01-01', CURRENT_TIMESTAMP()));
+INSERT INTO MOBILE_SUBSCRIPTION (msisdn, customer_id_owner, customer_id_user, service_type, service_start_date)
+  VALUES ('+35677143567', 1001, 1002, 'MOBILE_PREPAID', DATEDIFF('SECOND', DATE '1970-01-01', CURRENT_TIMESTAMP()));
